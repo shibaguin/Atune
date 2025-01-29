a33906b1fc8041caad649f4712c0317670f1788f
shibaguin <mizuguinalt@gmail.com>
2025-01-29 14:58:46 +0500

    Проект создан. CompanyName заменён на shibadev, Title=Atune.Android заменён на Atune. Изменены SDK 9.0 на 8.0 и net8.0-android34.0

---
+++ Atune/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Atune.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Atune/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Atune.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
