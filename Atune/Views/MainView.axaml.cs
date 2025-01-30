f1dc4f5568943de899bf4cf9b5988e2669b78feb
shibaguin <mizuguinalt@gmail.com>
2025-01-31 01:05:25 +0500

    Реализовано переключение темы кнопкой в шапке.

--- Atune/Views/MainView.axaml.cs
+++ Atune/Views/MainView.axaml.cs
@@ -1,5 +1,8 @@
+using System.IO;
+using Avalonia;
 using Avalonia.Controls;
-
+using Avalonia.Interactivity;
+using Avalonia.Styling;
 namespace Atune.Views;
 
 public partial class MainView : UserControl
@@ -7,5 +10,21 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+    }
+    
+    private void ToggleTheme(object sender, RoutedEventArgs e)
+    {
+        if (Application.Current is null) return;
+        
+        var newTheme = Application.Current.RequestedThemeVariant == ThemeVariant.Dark 
+            ? ThemeVariant.Light 
+            : ThemeVariant.Dark;
+        
+        Application.Current.RequestedThemeVariant = newTheme;
+    }
+    
+    private void OnSettingsClicked(object? sender, RoutedEventArgs e)
+    {
+        // Реализация метода
     }
 }
