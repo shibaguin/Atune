121a3bfdfda82086d5e5eb4f4dc20fc7eec9881d
shibaguin <mizuguinalt@gmail.com>
2025-04-06 10:04:35 +0300

    Accessing ContentResolver
    - Added - Context to MediaStorageService class to access ContentResolver
    - Implemented GetAllMediaItemsAsync method:
     - Use ContentResolver to request data from the media library
     - Request data about media files, including their IDs, paths and names
     - Create and add MediaItem objects to a list, which is then returned
    
    The MediaStorageService class can now retrieve data about media files from the Android media library.

--- Atune/Services/MediaStorageService.cs
+++ Atune/Services/MediaStorageService.cs
@@ -6,6 +6,10 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+#if ANDROID
+using Android.Content;
+using Android.Provider;
+#endif
 
 namespace Atune.Services
 {
@@ -13,11 +17,21 @@ namespace Atune.Services
     {
         private readonly AppDbContext _context;
         private readonly ILoggerService _logger;
+#if ANDROID
+        private readonly Context _context; // Добавлено для доступа к ContentResolver
+#endif
 
-        public MediaStorageService(AppDbContext context, ILoggerService logger)
+        public MediaStorageService(AppDbContext context, ILoggerService logger
+#if ANDROID
+            , Context context // Внедрение Context для Android
+#endif
+        )
         {
             _context = context;
             _logger = logger;
+#if ANDROID
+            this._context = context; // Инициализация контекста
+#endif
         }
 
         public async Task<string> GetMediaPathAsync(string mediaId)
@@ -33,7 +47,34 @@ namespace Atune.Services
 
         public async Task<List<MediaItem>> GetAllMediaItemsAsync()
         {
+#if ANDROID
+            var mediaItems = new List<MediaItem>();
+            var uri = MediaStore.Audio.Media.ExternalContentUri; // URI для медиатеки
+            var projection = new[] { MediaStore.Audio.Media.InterfaceConsts.Id, MediaStore.Audio.Media.InterfaceConsts.Data, MediaStore.Audio.Media.InterfaceConsts.Title };
+
+            using (var cursor = _context.ContentResolver.Query(uri, projection, null, null, null))
+            {
+                if (cursor != null)
+                {
+                    while (cursor.MoveToNext())
+                    {
+                        var id = cursor.GetLong(cursor.GetColumnIndexOrThrow(MediaStore.Audio.Media.InterfaceConsts.Id));
+                        var path = cursor.GetString(cursor.GetColumnIndexOrThrow(MediaStore.Audio.Media.InterfaceConsts.Data));
+                        var title = cursor.GetString(cursor.GetColumnIndexOrThrow(MediaStore.Audio.Media.InterfaceConsts.Title));
+
+                        mediaItems.Add(new MediaItem
+                        {
+                            Id = (int)id, // Приведение к int, если необходимо
+                            Path = path,
+                            Title = title
+                        });
+                    }
+                }
+            }
+            return mediaItems;
+#else
             return await _context.MediaItems.ToListAsync();
+#endif
         }
 
         public async Task<bool> MediaExistsAsync(string mediaId)
