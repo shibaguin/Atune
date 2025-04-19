cbd74878033a904ad2206f6e9c7e68e8a89cbf75
p1kqbb <p1kqbb@gmail.com>
2025-04-20 03:16:13 +0500

    fixes and improvements of TrackItemView.axaml.cs

--- Atune/Views/TrackItemView.axaml.cs
+++ Atune/Views/TrackItemView.axaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Input;
 using Atune.Models;
 using Microsoft.Extensions.DependencyInjection;
 using Atune.Services;
+using System.Collections.Generic;
+using Avalonia.Controls.ApplicationLifetimes;
+using Atune.ViewModels;
 
 namespace Atune.Views
 {
@@ -37,19 +40,33 @@ namespace Atune.Views
 
             // Find named controls
             var playBtn = this.FindControl<Button>("PlayButton");
+            var menuButton = this.FindControl<Button>("PlaylistMenuButton");
             var playlistMenu = this.FindControl<ContextMenu>("PlaylistContextMenu");
             var removeBtn = this.FindControl<Button>("RemoveButton");
 
-            // Wire up play button
+            // Wire up play button (fallback to MediaPlayerService if no PlayCommand)
             if (playBtn != null)
             {
-                playBtn.Click += (_, __) =>
-                    PlayCommand?.Execute(DataContext);
+                playBtn.Click += async (_, __) =>
+                {
+                    if (PlayCommand != null)
+                    {
+                        PlayCommand.Execute(DataContext);
+                    }
+                    else if (DataContext is MediaItem item)
+                    {
+                        var player = App.Current.Services.GetRequiredService<MediaPlayerService>();
+                        await player.StopAsync();
+                        await player.Play(item.Path);
+                    }
+                };
             }
 
-            // Populate playlists menu when opened
-            if (playlistMenu != null)
+            // Wire up playlist menu button to open the context menu
+            if (menuButton != null && playlistMenu != null)
             {
+                menuButton.Click += (_, __) => playlistMenu.Open(menuButton);
+                // Populate playlists menu when opened
                 playlistMenu.Opened += PlaylistContextMenu_Opened;
             }
 
@@ -79,20 +96,35 @@ namespace Atune.Views
 
         private async void PlaylistContextMenu_Opened(object? sender, RoutedEventArgs e)
         {
-            if (sender is not ContextMenu menu || AddToPlaylistCommand == null)
+            if (sender is not ContextMenu menu)
                 return;
 
             // Retrieve all playlists from the service
             var service = App.Current.Services.GetRequiredService<IPlaylistService>();
             var playlists = await service.GetPlaylistsAsync();
 
-            // Build menu items
-            menu.ItemsSource = playlists.Select(pl => new MenuItem
+            // Build menu items with click handlers to add current track
+            var items = new List<MenuItem>();
+            foreach (var pl in playlists)
             {
-                Header = pl.Name,
-                Command = AddToPlaylistCommand,
-                CommandParameter = pl
-            });
+                var mi = new MenuItem { Header = pl.Name };
+                mi.Click += async (_, __) =>
+                {
+                    if (DataContext is MediaItem track)
+                    {
+                        try
+                        {
+                            await service.AddToPlaylistAsync(pl.Id, track.Id);
+                        }
+                        catch
+                        {
+                            // Optionally log error
+                        }
+                    }
+                };
+                items.Add(mi);
+            }
+            menu.ItemsSource = items;
         }
     }
 } 
